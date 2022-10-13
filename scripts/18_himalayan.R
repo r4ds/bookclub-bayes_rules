258dca2933ad47da137a71736d2d3df01498b2f1
Federica Gazzelloni <docksbox@pm.me>
2022-10-13 16:36:58 +0200

    chapter18

---
+++ scripts/18_himalayan.R
@@ -0,0 +1,17 @@
+
+library(tidyverse)
+
+
+
+
+members <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-22/members.csv')
+expeditions <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-22/expeditions.csv')
+peaks <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-22/peaks.csv')
+
+
+
+
+data(climbers_sub)
+climbers <- climbers_sub %>% 
+  select(expedition_id, member_id, success, year, season,
+         age, expedition_role, oxygen_used)
